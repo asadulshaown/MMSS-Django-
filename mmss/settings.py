995376ef96d36c54c1578c045664c460e467a254
asa <asa27403@gmail.com>
2025-08-02 01:47:52 +0600

    change setting.py

--- mmss/settings.py
+++ mmss/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q6k0egjsiua!n$wbs!zgyaa250jz1wo!i*(kzxjs^r&s46%t2g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+
 
 
 # Application definition
