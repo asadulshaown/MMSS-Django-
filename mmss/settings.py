bcfa882b1353d67328b87bcad8a56bf0c7a18634
asa <asa27403@gmail.com>
2025-08-02 21:40:34 +0600

    fiix some isue

--- mmss/settings.py
+++ mmss/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q6k0egjsiua!n$wbs!zgyaa250jz1wo!i*(kzxjs^r&s46%t2g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
